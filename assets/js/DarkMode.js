d16562014bcfb4cdb87da449ddcb413226828ed5
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2025-10-09 16:44:08 -0600

    Delete assets directory

--- assets/js/DarkMode.js
+++
@@ -1,6 +0,0 @@
-const DarkMode = document.querySelector('button');
-const body = document.querySelector('body');
-function DarkMode() {
-    // let body = document.getElementByTagName('body')[0];
-    body.style.backgroundColor = '#000000';
-}
